0ff90c2888a809a285b3ebb7ca3e0b5d95ddf24d
Julianxalessio <julianxalessio@gmail.com>
2026-04-14 10:35:43 +0200

    eval

--- eval.java
+++ eval.java
@@ -1,9 +1,29 @@
-
 public class eval {
-    public static String calculate() {
-        int a = 5;
-        int b = 10;
-        int sum = a + b;
-        return "The sum of " + a + " and " + b + " is: " + sum;
+    public static void main(String[] args) {
+        String input = "3+5*2-8/4.445";
+        String result = calculate(input);
+        System.out.println("Result: " + result);
+    }
+
+    public static String calculate(String input) {
+        String[] tokens = input.split("(?<=[+\\-*/])|(?=[+\\-*/])");
+
+        float result = 0;
+        char lastOperator = '+';
+
+        for (String token : tokens) {
+            if (token.matches("[+\\-*/]")) {
+                lastOperator = token.charAt(0);
+            } else {
+                float number = Float.parseFloat(token);
+
+                if (lastOperator == '+') result += number;
+                else if (lastOperator == '-') result -= number;
+                else if (lastOperator == '*') result *= number;
+                else if (lastOperator == '/') result /= number;
+            }
+        }
+
+        return String.valueOf(result);
     }
 }
