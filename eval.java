3793651583d2dff06fb847de0784cefbc2a3a936
Julianxalessio <julianxalessio@gmail.com>
2026-04-14 10:20:06 +0200

    init

---
+++ eval.java
@@ -0,0 +1,9 @@
+
+public class eval {
+    public static String calculate() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        return "The sum of " + a + " and " + b + " is: " + sum;
+    }
+}
